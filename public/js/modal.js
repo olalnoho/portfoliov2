553d15051363ce55ae374f0005618ffb77c285ef
unknown <alexander.norberghoff@gmail.com>
2020-02-10 19:06:49 +0100

    added filter logic

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,4 +1,4 @@
-const btn = document.querySelector('.project__button')
+const btn = document.querySelectorAll('.project__button')
 const modal = document.querySelector('.modal')
 const backdrop = document.querySelector('.backdrop')
 
@@ -6,7 +6,8 @@ backdrop.addEventListener('click', function (e) {
    modal.classList.toggle('closed')
    this.classList.toggle('closed')
 })
-btn.addEventListener('click', function (e) {
+
+Array.from(btn).forEach(el => el.addEventListener('click', function (e) {
    modal.classList.toggle('closed')
    backdrop.classList.toggle('closed')
-})
+}))
