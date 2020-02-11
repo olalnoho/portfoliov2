e6fa5946b48cf3f5f275105d0c7384ed80852734
unknown <alexander.norberghoff@gmail.com>
2020-02-11 16:18:32 +0100

    more modal work

--- public/js/modalData.js
+++ public/js/modalData.js
@@ -5,12 +5,13 @@ const modalData = {
          'A small social media network with profiles, a message board and private messaging',
       link: 'https://alho-socialmedia.herokuapp.com',
       source: '#!',
-      images: ['./images/sms/0.png', './images/sms/1.png', './images/sms/2.png']
+      images: ['./images/sms/0.png', './images/sms/1.png', './images/sms/2.png', './images/sms/3.png', './images/sms/4.png']
    },
    cmdNote: {
       title: 'Note and todo collector',
       detail: 'Goes through all js files in current working directory and all it\'s sub-directories and collects comments that start with @note or @todo',
-      source: '#!'
+      source: '#!',
+      images: ['./images/cmdNote/0.png']
    },
    blog: {
       title: 'Blog App',
